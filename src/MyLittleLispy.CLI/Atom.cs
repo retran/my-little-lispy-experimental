4b57167f4fc66cd732335c7ccd73481c17a4c497
U-INT\avasilyev <avasilyev@msk-ws-39.int.acumatica.com>
2014-01-27 11:24:46 +0400

    rename to my-little-lispy

---
+++ src/MyLittleLispy.CLI/Atom.cs
@@ -0,0 +1,14 @@
+namespace MyLittleLispy.CLI
+{
+    public class Atom : Node
+    {
+        public override dynamic Eval(Context context)
+        {
+            if (Value is string && !Quote)
+            {
+                return context.Lookup(Value);
+            }
+            return Value;
+        }
+    }
+}
---
+++ src/MyLittleLispy.CLI/LocalContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MyLittleLispy.CLI
+{
+    public class LocalContext
+    {
+        private readonly Dictionary<string, dynamic> _locals;
+
+        public LocalContext(Context context, IEnumerable<string> args, IEnumerable<Node> argValues)
+        {
+            _locals = new Dictionary<string, dynamic>();
+            foreach (var pair in args.Zip(argValues, (s, node) => new KeyValuePair<string, Node>(s, node)))
+            {
+                _locals.Add(pair.Key, pair.Value.Eval(context));
+            }
+        }
+
+        public dynamic Lookup(string name)
+        {
+            return _locals[name];
+        }
+    }
+}
