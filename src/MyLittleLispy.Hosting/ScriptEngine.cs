7694a7e68d42ef4bb12ba3d4c2710ffe53b50dbc
Andrew Vasilyev <a.vasilyev@corvusalba.ru>
2015-01-31 09:58:31 +0300

    add ScriptEngine.Execute

--- src/MyLittleLispy.Hosting/ScriptEngine.cs
+++ src/MyLittleLispy.Hosting/ScriptEngine.cs
@@ -1,4 +1,6 @@
-﻿using MyLittleLispy.Runtime;
+﻿using System.IO;
+using System.Text;
+using MyLittleLispy.Runtime;
 
 namespace MyLittleLispy.Hosting
 {
@@ -19,6 +21,38 @@ namespace MyLittleLispy.Hosting
 	public Value Evaluate(string line)
 	{
 	    return _parser.Parse(line).Eval(_context);
+	}
+
+	public Value Execute(Stream stream)
+	{
+	    var sr = new StreamReader(stream);
+	    var script = sr.ReadToEnd();
+	    
+	    Value result = Null.Value;	    
+	    var count = 0;
+
+	    var sb = new StringBuilder();
+	    for (var i = 0; i < script.Length; i++)
+	    {
+		sb.Append(script[i]);
+		if (script[i] == '(')
+		{
+		    count++;
+		}
+
+		if (script[i] == ')')
+		{
+		    count--;
+		}
+
+		if (count == 0)
+		{
+		    result = _parser.Parse(sb.ToString()).Eval(_context);
+		    sb = new StringBuilder(); // TODO как-то можно очистить?
+		}
+	    }
+
+	    return result;
 	}
     }
 }
