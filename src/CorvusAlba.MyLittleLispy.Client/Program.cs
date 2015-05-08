75c6e73aea99cd3e4e4ba602770fd0efde1ce8d9
Andrew Vasilyev <a.vasilyev@corvusalba.ru>
2015-05-09 00:09:16 +0300

    add simple command line parser and --inspect mode

--- src/CorvusAlba.MyLittleLispy.Client/Program.cs
+++ src/CorvusAlba.MyLittleLispy.Client/Program.cs
@@ -1,31 +1,81 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
+using System.Collections;
 using CorvusAlba.MyLittleLispy.Runtime;
 using CorvusAlba.MyLittleLispy.Hosting;
 
 namespace CorvusAlba.MyLittleLispy.Client
 {
+    internal class CommandLineArgs
+    {
+        public string Script = string.Empty;
+        public bool Inspect = false;
+
+        private IEnumerator _enumerator = null;
+        
+        public void Parse(string[] args)
+        {
+            _enumerator = args.GetEnumerator();
+            while (_enumerator.MoveNext() && _enumerator.Current != null)
+            {
+                Arg(_enumerator.Current.ToString());
+            }
+        }
+
+        public void Arg(string arg)
+        {
+            if (arg.StartsWith("-"))
+            {
+                if (arg.Equals("-i", StringComparison.OrdinalIgnoreCase)
+                    || arg.Equals("--inspect", StringComparison.OrdinalIgnoreCase))
+                {
+                    Inspect = true;
+                }
+            }
+            else
+            {
+                Script = arg;
+            }
+        }
+    }
+    
     internal class Program
     {
         private static int Main(string[] args)
         {
-            if (!args.Any())
+            var arguments = new CommandLineArgs();
+            arguments.Parse(args);
+
+            var scriptEngine = new ScriptEngine();
+            if (!string.IsNullOrEmpty(arguments.Script))
             {
-                return new Repl(new ScriptEngine()).Loop();
+                using (var stream = new FileStream(args[0], FileMode.Open))
+                {
+                    try
+                    {
+                        scriptEngine.Execute(stream, true);
+                        if (!arguments.Inspect)
+                        {
+                            return 0;
+                        }
+                    }
+                    catch (HaltException e)
+                    {
+                        if (!arguments.Inspect)
+                        {                       
+                            return e.Code;
+                        }
+                    }
+                }
             }
             
-            using (var stream = new FileStream(args[0], FileMode.Open))
+            if (arguments.Inspect || string.IsNullOrWhiteSpace(arguments.Script))
             {
-                try
-                {
-                    (new ScriptEngine()).Execute(stream, true);
-                    return 0;
-                }
-                catch (HaltException e)
-                {
-                    return e.Code;
-                }
+                return new Repl(scriptEngine).Loop();
             }
+
+            throw new InvalidOperationException();
         }
     }
 }
